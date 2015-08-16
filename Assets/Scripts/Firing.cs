be0ccd769cb6af402090eaf76ccf16b3d5624352
Zawayx Falconer <zfalconer@hotmail.com>
2015-08-15 22:17:50 -0500

    Added three guns, functionality on one of them (currently attached to player)

---
+++ Assets/Scripts/Firing.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Firing : MonoBehaviour {
+	public GameObject bullet;
+	public GameObject barrel;
+	public float speed = 20f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetAxis("Strike") > 0){
+			GameObject bulletClone = (GameObject) Instantiate(bullet, barrel.transform.position, barrel.transform.rotation);
+			bulletClone.GetComponent<Rigidbody>().velocity = barrel.transform.forward * speed;
+		}
+	}
+}
