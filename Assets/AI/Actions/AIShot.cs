9627e24a5e7dd598fe934a3691cf834ce299e390
Zawayx Falconer <zfalconer@hotmail.com>
2015-08-17 06:16:24 -0500

    results of getting really confused on how RAIN works

---
+++ Assets/AI/Actions/AIShot.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using RAIN.Action;
+using RAIN.Core;
+
+[RAINAction]
+public class AIShot : RAINAction
+{
+	public Firing enemyGun;
+    public override void Start(RAIN.Core.AI ai)
+    {
+        base.Start(ai);
+    }
+
+    public override ActionResult Execute(RAIN.Core.AI ai)
+    {
+        return ActionResult.SUCCESS;
+    }
+
+    public override void Stop(RAIN.Core.AI ai)
+    {
+        base.Stop(ai);
+    }
+}
